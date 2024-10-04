2cab832ea4f29ab02b14022c866e4c17bc8612db
Jiri Klic <web@jiriklic.com>
2024-10-04 22:23:54 +0100

    Move external/gdal submodule to gdal

--- build.py
+++ build.py
@@ -1,22 +1,62 @@
 import os
-import shutil
 import subprocess
 import sys
+from setuptools import setup, Extension, find_packages
+from setuptools.command.build_ext import build_ext
 
-def build():
-    # Run the standard build process
-    subprocess.check_call([sys.executable, '-m', 'build'])
+class CMakeExtension(Extension):
+    def __init__(self, name, sourcedir=""):
+        Extension.__init__(self, name, sources=[])
+        self.sourcedir = os.path.abspath(sourcedir)
 
-    # Copy GDAL libraries to the package directory
-    gdal_lib_dir = os.path.join('build', 'gdal_install')
-    target_dir = os.path.join('build', 'lib', 'gdal_binary')
-    
-    if os.path.exists(target_dir):
-        shutil.rmtree(target_dir)
-    
-    shutil.copytree(gdal_lib_dir, target_dir)
+class CMakeBuild(build_ext):
+    def run(self):
+        try:
+            out = subprocess.check_output(["cmake", "--version"])
+        except OSError:
+            raise RuntimeError(
+                "CMake must be installed to build the following extensions: " +
+                ", ".join(e.name for e in self.extensions))
 
-    print("GDAL libraries copied successfully.")
+        for ext in self.extensions:
+            self.build_extension(ext)
 
-if __name__ == '__main__':
-    build()
+    def build_extension(self, ext):
+        extdir = os.path.abspath(
+            os.path.dirname(self.get_ext_fullpath(ext.name)))
+        cmake_args = ["-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=" + extdir,
+                      "-DPYTHON_EXECUTABLE=" + sys.executable,
+                      "-DCMAKE_INSTALL_PREFIX=" + extdir]
+
+        cfg = "Debug" if self.debug else "Release"
+        build_args = ["--config", cfg]
+
+        cmake_args += ["-DCMAKE_BUILD_TYPE=" + cfg]
+        build_args += ["--", "-j2"]
+
+        env = os.environ.copy()
+        env["CXXFLAGS"] = f"{env.get('CXXFLAGS', '')} -DVERSION_INFO=\\\"{self.distribution.get_version()}\\\""
+        if not os.path.exists(self.build_temp):
+            os.makedirs(self.build_temp)
+        subprocess.check_call(["cmake", ext.sourcedir] + cmake_args,
+                              cwd=self.build_temp, env=env)
+        subprocess.check_call(["cmake", "--build", "."] + build_args,
+                              cwd=self.build_temp)
+        subprocess.check_call(["cmake", "--install", "."],
+                              cwd=self.build_temp)
+        print()  # Add an empty line for cleaner output
+
+setup(
+    name="gdal-binary",
+    version="0.1.0",
+    author="Jiri Klic",
+    author_email="web@deltodon.com",
+    description="GDAL binary wheels",
+    long_description="",
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
+    ext_modules=[CMakeExtension("gdal_binary")],
+    cmdclass=dict(build_ext=CMakeBuild),
+    zip_safe=False,
+    python_requires=">=3.10",
+)
